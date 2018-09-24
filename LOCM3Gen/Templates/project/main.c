08d37cad2192b18ab87a63b572dd415962b9efe4
Maxim Yudin <stibiu@yandex.ru>
2018-09-24 20:12:57 +0300

    Created common main.c file for all microcontroller families.

---
+++ LOCM3Gen/Templates/project/main.c
@@ -0,0 +1,21 @@
+﻿/*
+ * {$ProjectName$}
+ *
+ * Created at {$Date$} {$Time$}
+ * by {$UserName$}
+ *
+ * Target device: {$DeviceName$}
+ */
+
+#include "libopencm3/stm32/rcc.h"
+#include "libopencm3/stm32/gpio.h"
+
+int main(void)
+{
+  //
+
+  while(1)
+  {
+    //
+  }
+}
